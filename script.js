fefc27c2af5d0c4cfd99a4ae36046c0c2f330edc
Divyansh-hooda <150668190+Divyansh-hooda@users.noreply.github.com>
2026-07-17 03:00:45 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera,renderer.domElement);
 controls.enableDamping = true;
+scene.add(new THREE.AmbientLight(0xffffff,1));
+const pointLight = new THREE.pointLight(0xffffff,3);
+pointLight.position.set(10,10,10);
+scene.add(pointLight);
+const starsGeometry = new THREE.BufferGeometry();
+const starVertices = [];
+for(let i=0;i<2500,i++){
+    starVertices.push(
+        (Math.random()-0.5)*300,
+        (Math.random()-0.5)*300,
+        (Math.random()-0.5)*300
+    );
+}
